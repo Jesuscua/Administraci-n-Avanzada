14c16564943bf6994c897178208801a01169731d
Jesuscua <60193941+Jesuscua@users.noreply.github.com>
2020-10-18 16:47:10 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Mi primera página azure</title>
+</head>
+<body style="background-color:09HJ4S;">
+<h2 align="center"> Azure con github</h2>
+<hr>
+<p>mi prueba azure con github</p>
+</body>
+</html>
